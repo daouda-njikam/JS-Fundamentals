e71a603de236bf53aa44f16fc40d3d367fff2a16
NJIKAM LAMERE DAOUDA <daoudanjikam91@gmail.com>
2025-05-22 18:11:30 +0200

    Add 0-javascript_is_amazing.js

---
+++ exercicea.js
@@ -0,0 +1,11 @@
+// Déclaration de la variable avec une valeur (tu peux changer le nombre pour tester)
+let number = -5;
+
+// Vérification si le nombre est positif, négatif ou zéro
+if (number > 0) {
+  console.log(number + " is positive");
+} else if (number < 0) {
+  console.log(number + " is negative");
+} else {
+  console.log(number + " is zero");
+}
