4640720ea644cf4c59cefd3a35c488e158c123f8
NJIKAM LAMERE DAOUDA <daoudanjikam91@gmail.com>
2025-05-23 11:50:01 +0200

    Add script to print 'C is fun' multiple times or show error

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+const arg = process.argv[2];
+const count = parseInt(arg, 10);
+
+if (!arg || isNaN(count) || count <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  while (i < count) {
+    console.log("C is fun");
+    i++;
+  }
+}
+
