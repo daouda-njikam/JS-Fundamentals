908d525addd90dbee46997c43f2aeb49c72e5f1f
NJIKAM LAMERE DAOUDA <daoudanjikam91@gmail.com>
2025-05-23 11:45:45 +0200

    Add script to print multiple languages with a loop and one console.log

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+let i = 0;
+let output = "";
+
+while (i < messages.length) {
+  output += messages[i] + "\n";
+  i++;
+}
+
+console.log(output.trim());
